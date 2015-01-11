d5ceb580d9e313b0dc6839739d7e0e1ada8e49a2
wengjingying <wengjingying@gmail.com>
2015-01-12 01:03:08 +0800

    new version

--- plot3.R
+++ plot3.R
@@ -1,3 +1,4 @@
+
 eleurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 if(!file.exists("household_power_consumption.txt")){download.file(stormurl,"exdata-data-household_power_consumption.zip")}
 unzip("exdata-data-household_power_consumption.zip")
